4ce358b54965d480bc763d4d25e53477c03992c8
haroonkpk <haroonkhanlala47@gmail.com>
2026-08-24 16:45:29 +0500

    feat: refactor user schema for employee management and update admin dashboard layout

--- src/app/(protected)/admin/employees/page.tsx
+++ src/app/(protected)/admin/employees/page.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import { Header } from '@/components/layouts'
+import { Header } from "@/components/layouts";
 
 export default function AdminEmployeesPage() {
   return (
     <div className="min-h-screen bg-(--color-page-bg) sm:p-[clamp(1rem,3vw,2.5rem)] pb-24">
       <Header title="Employee & Staff Management" />
-      <main>
-        <p className="text-slate-600">Employee Management Content goes here...</p>
+      {/* Main Layout */}
+      <main className="flex flex-col-reverse md:flex-row gap-8">
+        {/* ── LEFT ── */}
+        <div className=" w-full max-w-3xl flex flex-col gap-4">hello</div>
+
+        {/*  RIGHT Side */}
+        <div className="lg:col-span-4">heloo</div>
       </main>
     </div>
-  )
+  );
 }
--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { LogoutButton } from '@/components/logout-button'
 
 interface HeaderProps {
